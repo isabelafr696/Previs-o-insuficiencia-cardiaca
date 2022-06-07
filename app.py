9eb6194e43e936d17c71207854aa0cc41f34c01c
isabelafr696 <105250155+isabelafr696@users.noreply.github.com>
2022-06-07 14:11:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,43 @@
-importar  cloudpickle
-from  flask  import  Flask , render_template , request
+import cloudpickle
+from flask import Flask, render_template, request
 
-com  open ( 'model.pkl' , 'rb' ) como  file_in :
-  modelo  =  cloudpickle . carregar ( arquivo_in )
+with open('model.pkl', 'rb') as file_in:
+  model = cloudpickle.load(file_in)
 
-app  =  Flask ( __name__ )
+app = Flask(__name__)
 
-@aplicativo . _ rota ( '/' )
-def  página inicial ():
-  return  render_template ( 'homepage.html' , nome = 'Fulano' )
+@app.route('/')
+def homepage():
+  return render_template('homepage.html', nome='Fulano')
 
-@aplicativo . _ route ( '/predicao' , métodos = [ 'POST' ])
-def  predicao ():
-  Gênero  =  int ( request . form [ 'Gender' ])
-  Senior_Citizen  =  int ( request . form [ 'Senior_Citizen' ])
-  Phone_Service  =  int ( request . form [ 'Phone_Service' ])
-  Tech_Support  =  int ( request . form [ 'Tech_Support' ])
-  Streaming_Movies  =  int ( request . form [ 'Streaming_Movies' ])
-  Contrato  =  int ( request . form [ 'Contract' ])
-  Payment_Method  =  int ( request . form [ 'Payment_Method' ])
-  predicao  =  modelo . prever ([ 'Gênero' ])
-  predicao  =  modelo . prever ([ 'Senior_Citizen' ])
-  predicao  =  modelo . prever ([ 'Phone_Service' ])
-  predicao  =  modelo . prever ([ 'Tech_Support' ])
-  predicao  =  modelo . prever ([ 'Streaming_Movies' ])
-  predicao  =  modelo . prever ([ 'Contrato' ])
-  predicao  =  modelo . prever ([ 'Payment_Method' ])
-  return  render_template ( 'predicao.html' , predicao = predicao [ 0 ])
+@app.route('/predicao', methods=['POST'])
+def predicao():
+  Gender = int(request.form['Gender'])
+  Senior_Citizen = int(request.form['Senior_Citizen'])
+  Phone_Service = int(request.form['Phone_Service'])
+  Tech_Support = int(request.form['Tech_Support'])
+  Streaming_Movies = int(request.form['Streaming_Movies'])
+  Contract = int(request.form['Contract'])
+  Payment_Method = int(request.form['Payment_Method'])
+  predicao = model.predict(['Gender'])
+  predicao = model.predict(['Senior_Citizen'])
+  predicao = model.predict(['Phone_Service'])
+  predicao = model.predict(['Tech_Support'])
+  predicao = model.predict(['Streaming_Movies'])
+  predicao = model.predict(['Contract'])
+  predicao = model.predict(['Payment_Method'])
+  return render_template('predicao.html', predicao=predicao[0])
   
   
-aplicativo . execute ( debug = True )
+app.run(debug=True)
 
-# pip install -r requirements.txt (instala como bibliotecas)
+# pip install -r requirements.txt (instala as bibliotecas)
 # python app.py (para executar)
 
-#git adicionar.
+# git add .
 
 # git commit -m "nomenovo"
-#git push
+# git push
+© 2022 GitHub, Inc.
+Terms
+Pri
